0458edecb5a135f29dbdf63a6f83bc4dc751577e
Jung's Study <JungsArk@gmail.com>
2025-09-15 16:51:37 +0900

    "SecurityConfig 수정, WithdrawController 매핑 주소 변경"

--- src/main/java/com/zerobase/fintech/controller/WithdrawController.java
+++ src/main/java/com/zerobase/fintech/controller/WithdrawController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/{accountId}/withdraws")
+@RequestMapping("/api/accounts/{accountId}/withdraws")
 @RequiredArgsConstructor
 public class WithdrawController {
 
